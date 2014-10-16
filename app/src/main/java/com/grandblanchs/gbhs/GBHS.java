1d0d58154d57323525dfc8bce4452527e689c300
Corey Rowe <CoreyR1196@gmail.com>
2014-10-15 20:51:52 -0400

    Created FragmentManager

--- app/src/main/java/com/grandblanchs/gbhs/GBHS.java
+++ app/src/main/java/com/grandblanchs/gbhs/GBHS.java
@@ -1,16 +1,31 @@
 package com.grandblanchs.gbhs;
 
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 
 
-public class GBHS extends Activity {
+public class GBHS extends Activity implements twitter.OnFragmentInteractionListener {
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    //Create FragmentManager for switching fragments
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gbhs);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        calendar calfragment = new calendar();
+        fragmentTransaction.add(R.id.FragmentContainer, calfragment);
+        fragmentTransaction.commit();
+
     }
 
 
@@ -29,8 +44,22 @@ public class GBHS extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         System.out.println("onOptionsItemSelected");
         int id = item.getItemId();
-        if (id == R.id.news) {
-            System.out.println("Giving you the news");
+        twitter twitterfragment = new twitter();
+        calendar calfragment = new calendar();
+        if (id == R.id.twitter) {
+            System.out.println("Switching to Twitter feed");
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, twitterfragment);
+            fragmentTransaction.commit();
+            return true;
+        }
+        if (id == R.id.calendar) {
+            System.out.println("Switching to Calendar");
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.FragmentContainer, calfragment);
+            fragmentTransaction.commit();
             return true;
         }
         return super.onOptionsItemSelected(item);
