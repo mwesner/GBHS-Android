fe6554a7e0ac099eada158eabad816160efb061c
Aaron Goodfellow <aamigo89@gmail.com>
2014-11-13 08:13:31 -0500

    Added code so that lst_feed is filled from a string[]

--- app/src/main/java/com/grandblanchs/gbhs/twitter.java
+++ app/src/main/java/com/grandblanchs/gbhs/twitter.java
@@ -10,11 +10,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import twitter4j.*;
+
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -22,44 +25,18 @@ import twitter4j.auth.AccessToken;
 
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link twitter.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link twitter#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class twitter extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
     //This is authentication for the first step of logging into Twitter
     private final static String CONSUMER_KEY = "0S62lfz7hGX39oZo2jJmrhZ96";
     private final static String CONSUMER_KEY_SECRET ="Pr1YnBtFU5OErrxhpLNet2S6KolhRm43cfwZuFPCQLOasEPXm7";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment twitter.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static twitter newInstance(String param1, String param2) {
+    ListView lst_feed;
+
+    public static twitter newInstance() {
         twitter fragment = new twitter();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
     public twitter() {
@@ -70,8 +47,6 @@ public class twitter extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -121,18 +96,8 @@ public class twitter extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
     }
 
@@ -150,19 +115,30 @@ public class twitter extends Fragment {
             AccessToken oathAccessToken = new AccessToken(accessToken, accessTokenSecret);
             twitter.setOAuthAccessToken(oathAccessToken);
 
-            List<twitter4j.Status> stati;
-
-
-            try {
-               stati  = twitter.getHomeTimeline();
-               System.out.println(stati) ;
-               ListView lst_feed = (ListView) getView().findViewById(R.id.lst_feed);
 
 
 
-            } catch (TwitterException e) {
-                e.printStackTrace();
-            }
+
+               lst_feed = (ListView) getView().findViewById(R.id.lst_feed);
+               final String [] testing = new String[] { "Number one", "Number two", "Number three", "Number four", "Number five"};
+
+            ArrayList<String> list = new ArrayList<String>();
+                for (int i = 0; i < testing.length; ++i) {
+                    list.add(testing[i]);
+                }
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                            android.R.layout.simple_list_item_1, testing);
+                    lst_feed.setAdapter(adapter);
+
+                }
+            });
+
+
+
 
             return null;
         }
