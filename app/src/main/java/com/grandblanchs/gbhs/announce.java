01dbf1157d56e28d3a4f8bdfd0e99191b3f50add
Corey Rowe <CoreyR1196@gmail.com>
2015-02-12 08:23:06 -0500

    Added primitive announcements scraper

--- app/src/main/java/com/grandblanchs/gbhs/announce.java
+++ app/src/main/java/com/grandblanchs/gbhs/announce.java
@@ -19,6 +19,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class announce extends Fragment {
 
@@ -77,36 +80,75 @@ public class announce extends Fragment {
 
     public void onStart() {
         super.onStart();
+
         prog = (ProgressBar) getView().findViewById(R.id.prog);
         lstAnnounce = (ListView) getView().findViewById(R.id.lstAnnounce);
-
         new AnnounceScrape().execute();
     }
 
     public class AnnounceScrape extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
+            //Scrape the daily announcements into a list.
             Document announce = null;
+            Document rss = null;
             context = getActivity().getApplicationContext();
+            List<String> list;
+
             try {
                 announce = Jsoup.connect("http://grandblanc.high.schoolfusion.us/modules/cms/pages.phtml?pageid=22922").get();
                 Elements announceClass = announce.getElementsByClass("MsoNormal").tagName("li");
 
-                announceTextArray = announceClass.text().split("</li>");
-                displayArray = new String[announceTextArray.length];
-                for (int i = 0; i < announceTextArray.length; i++) {
-                    displayArray[i] = announceTextArray[i].substring(22, announceTextArray[i].length());
+                //Split by class/bullet point
+                announceTextArray = announceClass.toString().split("</li>");
+
+                if (announceClass.isEmpty()) {
+                    list = new ArrayList<String>();
+
+                    //Add "No Announcements."
+                    list.add(0, "No announcements.");
+
+                }else{
+                    displayArray = new String[announceTextArray.length];
+                    for (int i = 0; i < announceTextArray.length; i++) {
+                        //Populate the array, removing the class and style tags
+
+                        displayArray[i] = announceTextArray[i].substring(85, announceTextArray[i].length());
+
+                        //Overwrite undesired HTML characters
+                        displayArray[i] = displayArray[i].replaceAll("&nbsp;", "");
+                        displayArray[i] = displayArray[i].replaceAll("&amp;", "");
+                        displayArray[i] = displayArray[i].replaceAll("<br>", "");
+                        displayArray[i] = displayArray[i].replaceAll("</strong></span>", "");
+                        displayArray[i] = displayArray[i].replaceAll("ext-align: center;\">  <span style=\"font-size:16px;\"><strong>", "");
+                    }
+
+
+                    //Convert to ArrayList for easy item removal
+                    list = new ArrayList<String>(Arrays.asList(displayArray));
+
+                    //Remove the first three garbage entries
+                    list.remove(0);
+                    list.remove(1);
+                    list.remove(2);
                 }
+
                 adapter = new ArrayAdapter<String>(context,
-                        android.R.layout.simple_list_item_1, displayArray);
+                        android.R.layout.simple_list_item_1, list);
             } catch (IOException e) {
                 getActivity().runOnUiThread(new Runnable() {
                     public void run() {
                         Toast.makeText(context, getString(R.string.NoConnection), Toast.LENGTH_LONG).show();
                     }
                 });
-            }
+            } catch (NullPointerException e) {
+                list = new ArrayList<String>();
 
+                //Add "No Announcements."
+                list.add(0, "No announcements.");
+
+                Toast.makeText(context, "NullPointer warning", Toast.LENGTH_SHORT).show();
+            }
             return null;
         }
 
