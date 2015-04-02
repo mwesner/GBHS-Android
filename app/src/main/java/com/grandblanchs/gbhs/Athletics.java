9af3ad426aa6695444111341afbe6fc5aa5ccd19
Corey Rowe <CoreyR1196@gmail.com>
2015-04-02 19:54:36 -0400

    Sports now show correct level and gender options

--- app/src/main/java/com/grandblanchs/gbhs/Athletics.java
+++ app/src/main/java/com/grandblanchs/gbhs/Athletics.java
@@ -2,13 +2,14 @@ package com.grandblanchs.gbhs;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
@@ -21,9 +22,11 @@ import java.util.Locale;
 
 public class Athletics extends Fragment {
 
-    //TODO: (Corey) Add athletic events into a list that shows on date change
+    //TODO: (Corey) Show season schedule for selected athletic event
 
     private OnFragmentInteractionListener mListener;
+
+    Context context;
 
     ProgressBar prog;
     Spinner lstSport;
@@ -36,6 +39,35 @@ public class Athletics extends Fragment {
     boolean genderPicked;
 
     String baseUrl;
+    Calendar cal;
+    SimpleDateFormat sdf;
+
+    String[] levelselect;
+    String[] genderselect;
+
+    String[] sports;
+    String[] levelall;
+    String[] varsity;
+    String[] juniorvarsity;
+    String[] genderall;
+    String[] boys;
+    String[] girls;
+    String[] boysgirls;
+    String[] combined;
+
+    int levelStatus;
+    /*0 = all
+     *1 = V
+     *2 = JV
+     */
+
+    int genderStatus;
+    /*0 = all
+     *1 = boys
+     *2 = girls
+     *3 = boysgirls
+     *4 = combined
+     */
 
     public Athletics() {
         // Required empty public constructor
@@ -76,11 +108,25 @@ public class Athletics extends Fragment {
     public void onStart() {
         super.onStart();
         if (getActivity() != null) {
+            context = getActivity().getApplicationContext();
+
             prog = (ProgressBar) getView().findViewById(R.id.progCalendar);
             lstSport = (Spinner) getView().findViewById(R.id.lstSport);
             lstLevel = (Spinner) getView().findViewById(R.id.lstLevel);
             lstGender = (Spinner) getView().findViewById(R.id.lstGender);
             btnSport = (Button) getView().findViewById(R.id.btnSport);
+
+            levelselect = getActivity().getResources().getStringArray(R.array.sportslevelselect); //Clears level options
+            genderselect = getActivity().getResources().getStringArray(R.array.sportsgenderselect); //Clears gender options
+            sports = getActivity().getResources().getStringArray(R.array.sports); //List of sports
+            levelall = getActivity().getResources().getStringArray(R.array.sportslevelall); //Varsity, JV, and Freshman
+            varsity = getActivity().getResources().getStringArray(R.array.sportslevelvarsity); //Varsity only
+            juniorvarsity = getActivity().getResources().getStringArray(R.array.sportsleveljuniorvarsity); //Varsity and JV only
+            genderall = getActivity().getResources().getStringArray(R.array.sportsgenderall); //Boys, Girls, and "Boys and Girls"
+            boysgirls = getActivity().getResources().getStringArray(R.array.sportsgenderboysgirls); //Only Boys and Girls
+            boys = getActivity().getResources().getStringArray(R.array.sportsgenderboys); //Only Boys
+            girls = getActivity().getResources().getStringArray(R.array.sportsgendergirls);  //Only Girls
+            combined = getActivity().getResources().getStringArray(R.array.sportsgendercombined); //Only "Boys and Girls"
 
             lstSport.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
@@ -89,9 +135,9 @@ public class Athletics extends Fragment {
                         sportPicked = false;
                         toggleSportButton();
                     } else {
-                        btnSport.setEnabled(true);
                         sportPicked = true;
                         toggleSportButton();
+                        checkSport();
                     }
                 }
 
@@ -143,19 +189,6 @@ public class Athletics extends Fragment {
             btnSport.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(
-                            getActivity().getApplicationContext(),
-                            getResources().getStringArray(R.array.sportsgender)[lstGender.getSelectedItemPosition()]
-                            +" " + getResources().getStringArray(R.array.sportslevel)[lstLevel.getSelectedItemPosition()]
-                            + " " + getResources().getStringArray(R.array.sports)[lstSport.getSelectedItemPosition()],
-                            Toast.LENGTH_LONG)
-                            .show();
-                    btnSport.setText(R.string.SportSelect);
-                    lstSport.setVisibility(View.GONE);
-                    lstLevel.setVisibility(View.GONE);
-                    lstGender.setVisibility(View.GONE);
-
-                    prog.setVisibility(View.VISIBLE);
 
                     new getSeasonSchedule().execute();
                 }
@@ -171,25 +204,266 @@ public class Athletics extends Fragment {
         }
     }
 
+    public void checkSport() {
+        //Change the options available based on the selected sport.
+
+        switch (lstSport.getSelectedItemPosition()) {
+            case 0: //Reset
+                clearOptions();
+            case 1: //Baseball: All levels, Boys
+                setLevelAll();
+                setBoys();
+                break;
+            case 2: //Basketball: All levels, Boys, Girls
+                setLevelAll();
+                setBoysGirls();
+                break;
+            case 3: //Bowling: Varsity, Combined
+                setVarsity();
+                setCombined();
+                break;
+            case 4: //Cheerleading: All levels, Girls
+                setLevelAll();
+                setGirls();
+                break;
+            case 5: //Cross Country: Varsity, all genders
+                setVarsity();
+                setGenderAll();
+                break;
+            case 6: //Football: All levels, Boys
+                setLevelAll();
+                setBoys();
+                break;
+            case 7: //Golf: Varsity and JV, Boys, Girls
+                setJuniorVarsity();
+                setBoysGirls();
+                break;
+            case 8: //Ice Hockey: Varsity, Boys
+                setVarsity();
+                setBoys();
+                break;
+            case 9: //Lacrosse: Varsity and JV, Boys, Girls
+                setJuniorVarsity();
+                setBoysGirls();
+                break;
+            case 10: //Skiing: Varsity and JV, Combined
+                setJuniorVarsity();
+                setCombined();
+                break;
+            case 11: //Soccer: All levels, Boys, Girls
+                setLevelAll();
+                setBoysGirls();
+                break;
+            case 12: //Softball: All levels, Girls
+                setLevelAll();
+                setGirls();
+                break;
+            case 13: //Swim and Dive: Varsity, Boys, Girls
+                setVarsity();
+                setBoysGirls();
+                break;
+            case 14: //Tennis: Varsity, JV, Boys, Girls
+                setJuniorVarsity();
+                setBoysGirls();
+                break;
+            case 15: //Track: Varsity, Boys, Girls
+                setVarsity();
+                setBoysGirls();
+                break;
+            case 16: //Volleyball: All levels, Girls
+                setLevelAll();
+                setGirls();
+                break;
+            case 17: //Water Polo: Varsity, JV, Boys, Girls
+                setJuniorVarsity();
+                setBoysGirls();
+                break;
+        }
+
+    }
+
+    public void clearOptions() {
+        ArrayAdapter levelAdapter = new ArrayAdapter<>(
+            context,
+            android.R.layout.simple_spinner_dropdown_item,
+            levelselect);
+
+        ArrayAdapter genderAdapter = new ArrayAdapter<>(
+            context,
+            android.R.layout.simple_spinner_dropdown_item,
+            genderselect);
+
+        lstLevel.setAdapter(levelAdapter);
+        lstGender.setAdapter(genderAdapter);
+
+    }
+
+
+    public void setLevelAll() {
+
+        levelStatus = 1;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                levelall);
+
+        lstLevel.setAdapter(spinnerAdapter);
+    }
+
+    public void setVarsity() {
+
+        levelStatus = 2;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                varsity);
+
+        lstLevel.setAdapter(spinnerAdapter);
+    }
+
+    public void setJuniorVarsity() {
+
+        levelStatus = 3;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                juniorvarsity);
+
+        lstLevel.setAdapter(spinnerAdapter);
+    }
+
+    public void setGenderAll() {
+
+        genderStatus = 1;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                genderall);
+
+        lstGender.setAdapter(spinnerAdapter);
+    }
+
+    public void setBoys() {
+
+        genderStatus = 2;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                boys);
+
+        lstGender.setAdapter(spinnerAdapter);
+    }
+
+    public void setGirls() {
+
+        genderStatus = 3;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                girls);
+
+        lstGender.setAdapter(spinnerAdapter);
+    }
+
+    public void setBoysGirls() {
+
+        genderStatus = 4;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                boysgirls);
+
+        lstGender.setAdapter(spinnerAdapter);
+    }
+
+    public void setCombined() {
+
+        genderStatus = 5;
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter<>(
+                context,
+                android.R.layout.simple_spinner_dropdown_item,
+                combined);
+
+        lstGender.setAdapter(spinnerAdapter);
+    }
+
     class getSeasonSchedule extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
 
             /**SCHEDULE SCRAPER**/
 
-            //Get the season event page.
-            baseUrl = "http://schedules.schedulestar.com/Grand-Blanc-High-School-Grand-Blanc-MI/season";
+            //Construct URL based on user selections
+
+            /*URL structure:
+            http://schedules.schedulestar.com/Grand-Blanc-High-School-Grand-Blanc-MI/season/[MM-dd-yyyy]/[Gender]/[Level]/[Sport]
+            */
 
             //Add the current date (MM-DD-YYYY).
-            Calendar cal = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
+            cal = Calendar.getInstance();
+            sdf = new SimpleDateFormat("MM-dd-yyyy", Locale.US);
 
-            baseUrl += "/" + sdf.format(cal.getTime());
-            baseUrl += "/" + getResources().getStringArray(R.array.sportsgender)[lstGender.getSelectedItemPosition()];
-            baseUrl += "/" + getResources().getStringArray(R.array.sportslevel)[lstLevel.getSelectedItemPosition()];
-            baseUrl += "/" + getResources().getStringArray(R.array.sports)[lstSport.getSelectedItemPosition()];
+            //Get the base season event page.
+            baseUrl = getString(R.string.BaseURL) + "/" + sdf.format(cal.getTime());
 
-            Log.d("URL", baseUrl);
+            //Add the gender to the URL constructor.
+            switch (genderStatus) {
+                case 1:
+                    baseUrl += "/" + genderall[lstGender.getSelectedItemPosition()];
+                    break;
+                case 2:
+                    baseUrl += "/" + boys[lstGender.getSelectedItemPosition()];
+                    break;
+                case 3:
+                    baseUrl += "/" + girls[lstGender.getSelectedItemPosition()];
+                    break;
+                case 4:
+                    baseUrl += "/" + boysgirls[lstGender.getSelectedItemPosition()];
+                    break;
+                case 5:
+                    baseUrl += "/" + combined[lstGender.getSelectedItemPosition()];
+                    break;
+            }
+
+            //Add the level to the URL constructor.
+            switch (levelStatus) {
+                case 1:
+                    baseUrl += "/" + levelall[lstLevel.getSelectedItemPosition()];
+                    break;
+                case 2:
+                    baseUrl += "/" + varsity[lstLevel.getSelectedItemPosition()];
+                    break;
+                case 3:
+                    baseUrl += "/" + juniorvarsity[lstLevel.getSelectedItemPosition()];
+                    break;
+            }
+
+            //Add the sport to the URL constructor.
+            baseUrl += "/" + sports[lstSport.getSelectedItemPosition()];
+
+            //Remove any spaces in the URL.
+            baseUrl = baseUrl.replace(" ", "%20");
+
+            if (getActivity() != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(
+                                context,
+                                baseUrl,
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
 
             return null;
         }
