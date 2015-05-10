1c6d5765eec60a41c76c2b7d39da8d06c760f697
Corey Rowe <CoreyR1196@gmail.com>
2015-05-10 13:34:03 -0400

    Added SlidingTabLayout, SlidingTabStrip, and ViewPagerAdapter Fixed drawable names

---
+++ app/src/main/java/com/grandblanchs/gbhs/TimesFull.java
@@ -0,0 +1,16 @@
+package com.grandblanchs.gbhs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TimesFull extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.times_full,container,false);
+    }
+}
