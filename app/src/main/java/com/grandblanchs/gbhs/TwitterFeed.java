e96b7672eee982f9aa4116de3e6a1c1b92cafcee
Corey Rowe <CoreyR1196@gmail.com>
2015-05-18 14:50:20 -0400

    Updated Twitter and Crashlytics versions Removed facebook and location dependencies

--- app/src/main/java/com/grandblanchs/gbhs/TwitterFeed.java
+++ app/src/main/java/com/grandblanchs/gbhs/TwitterFeed.java
@@ -1,28 +1,23 @@
 package com.grandblanchs.gbhs;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
-
-import io.fabric.sdk.android.Fabric;
 
 public class TwitterFeed extends Fragment {
 
     public interface OnFragmentInteractionListener{}
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.twitter, container, false);
+        return inflater.inflate(R.layout.fragment_twitter, container, false);
     }
 
     @Override
