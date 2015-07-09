d87b0b2d318bfb07128d5e900f25f456e8d5d211
Corey Rowe <CoreyR1196@gmail.com>
2015-07-09 14:13:12 -0400

    Implemented swipe to refresh in TwitterFragment.java

--- app/src/main/java/com/grandblanchs/gbhs/TwitterFragment.java
+++ app/src/main/java/com/grandblanchs/gbhs/TwitterFragment.java
@@ -1,16 +1,20 @@
 package com.grandblanchs.gbhs;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
 
-public class TwitterFragment extends Fragment {
+public class TwitterFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,16 +26,36 @@ public class TwitterFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        ListView timeline = (ListView) view.findViewById(R.id.timelinelist);
+        final SwipeRefreshLayout swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
+        final UserTimeline timeline = new UserTimeline.Builder().screenName("GrandBlancPride").build();
+        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter(getActivity(), timeline);
+        setListAdapter(adapter);
 
-        final UserTimeline userTimeline = new UserTimeline.Builder()
-                .screenName("GrandBlancPride")
-                .build();
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onRefresh() {
+                swipeLayout.setRefreshing(true);
+                adapter.refresh(new Callback() {
+                    @Override
+                    public void success(Result result) {
+                        swipeLayout.setRefreshing(false);
+                    }
 
-
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter(getActivity(),
-                userTimeline);
-        timeline.setAdapter(adapter);
+                    @Override
+                    public void failure(TwitterException exception) {
+                        swipeLayout.setRefreshing(false);
+                        Snackbar.make(getView(), getString(R.string.NoTwitterRefresh), Snackbar.LENGTH_LONG)
+                                .setAction(R.string.Retry, new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        onRefresh();
+                                    }
+                                })
+                                .show();
+                    }
+                });
+            }
+        });
     }
 }
-
