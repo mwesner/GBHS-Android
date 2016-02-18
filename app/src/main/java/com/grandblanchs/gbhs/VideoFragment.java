0605e285d65a2b373ffdd0a213cb0d2fd10e8b69
molmsted98 <molmsted98@gmail.com>
2016-02-18 13:04:55 -0500

    Began implementing Vimeo section.

---
+++ app/src/main/java/com/grandblanchs/gbhs/VideoFragment.java
@@ -0,0 +1,22 @@
+package com.grandblanchs.gbhs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VideoFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
