e03d88b6c00e0196dbf407fca40d5c7035579123
Corey Rowe <CoreyR1196@gmail.com>
2016-02-26 02:53:53 -0500

    Map now saves position on rotation

--- app/src/main/java/com/grandblanchs/gbhs/MapFragment.java
+++ app/src/main/java/com/grandblanchs/gbhs/MapFragment.java
@@ -2,6 +2,8 @@ package com.grandblanchs.gbhs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,39 +22,63 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private SupportMapFragment mapFragment;
+    private GoogleMap map;
 
     private Button btnWest;
     private Button btnEast;
     private Spinner lstType;
     private CameraUpdate cam;
 
-    private String[] maptype;
+    private int mapType;
+    private double latitude = 42.921275;
+    private double longitude = -83.627256;
+    private float zoom = 16;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        if (savedInstanceState != null) {
+            mapType = savedInstanceState.getInt("mapType");
+            latitude = savedInstanceState.getDouble("latitude");
+            longitude = savedInstanceState.getDouble("longitude");
+            zoom = savedInstanceState.getFloat("zoom");
+        }
 
-        maptype = getResources().getStringArray(R.array.maptype);
+        View v = null;
 
-        btnWest = (Button) v.findViewById(R.id.btnWest);
-        btnEast = (Button) v.findViewById(R.id.btnEast);
-        lstType = (Spinner) v.findViewById(R.id.lstType);
+        try {
+            v = inflater.inflate(R.layout.fragment_map, container, false);
 
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+            String[] maptypes = getResources().getStringArray(R.array.maptype);
+
+            btnWest = (Button) v.findViewById(R.id.btnWest);
+            btnEast = (Button) v.findViewById(R.id.btnEast);
+            lstType = (Spinner) v.findViewById(R.id.lstType);
+
+            lstType.setAdapter(new MapAdapter(getActivity(), maptypes));
+
+        }catch (InflateException e) {
+            //Map is already present.
+        }
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         return v;
     }
 
     @Override
-    public void onMapReady(final GoogleMap map) {
+    public void onMapReady(final GoogleMap googleMap) {
+        map = googleMap;
+
         btnEast.setVisibility(View.VISIBLE);
         btnWest.setVisibility(View.VISIBLE);
         lstType.setVisibility(View.VISIBLE);
 
         // Updates the location and zoom of the MapView
-        cam = CameraUpdateFactory.newLatLngZoom(new LatLng(42.921275, -83.627256), 16);
+        cam = CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), zoom);
         map.moveCamera(cam);
+
+        map.setMapType(mapType);
 
         final LatLng east = new LatLng(42.91995, -83.624859);
         final LatLng west = new LatLng(42.920577, -83.630655);
@@ -66,25 +92,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 .snippet(getString(R.string.West))
                 .position(west));
 
-        lstType.setAdapter(new MapAdapter(getActivity(), maptype));
-
         lstType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                        break;
-                    case 1:
-                        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                        break;
-                    case 2:
-                        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                        break;
-                    case 3:
-                        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                        break;
-                }
+                setMapType(position);
             }
 
             @Override
@@ -108,9 +119,49 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         });
     }
 
+    private void setMapType(int position) {
+        switch (position) {
+            case 0:
+                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case 1:
+                map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case 2:
+                map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case 3:
+                map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (map != null) {
+            outState.putInt("mapType", map.getMapType());
+            outState.putDouble("latitude", map.getCameraPosition().target.latitude);
+            outState.putDouble("longitude", map.getCameraPosition().target.longitude);
+            outState.putFloat("zoom", map.getCameraPosition().zoom);
+        }
+    }
+
     @Override
     public void onResume() {
         mapFragment.onResume();
         super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        mapFragment.onDestroyView();
+        super.onDestroyView();
+
+        Fragment fragment = getChildFragmentManager().findFragmentById(R.id.map);
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        ft.remove(fragment);
+        ft.commitAllowingStateLoss();
     }
 }
